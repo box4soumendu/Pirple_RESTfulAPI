44aef2420a9e9a5663869d26e79d2662ac1c44e6
Carlos Rodríguez <carlos.rguezgtz@gmail.com>
2019-10-06 03:15:40 -0500

    Parsing playloads

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 var http = require('http')
 var url = require('url')
+var StringDecoder = require('string_decoder').StringDecoder
 
 //  The server should respond to all request with a string
 var server = http.createServer(function (req, res) {
@@ -24,12 +25,22 @@ var server = http.createServer(function (req, res) {
     // Get the headers as an object
     var headers = req.headers
 
-    // Send the response
-    res.end('Hello World!\n')
-
-    // Log the request path
-    console.log('Request received on path: '+trimedPath+' - Method: '+method+' and with these query parameters', queryStringObject)
-    console.log('Headers of the request', headers)
+    // Get the playload, if any
+    var decoder = new StringDecoder('utf-8')
+    var buffer = ''
+    req.on('data', function (data) {
+        buffer += decoder.write(data)
+    })
+    req.on('end', function () {
+        buffer += decoder.end()
+        // The request has finished
+        // Send the response
+        res.end('Hello World!\n')
+        console.log('Request recieved wwith this playload: ', buffer)
+        // Log the request path
+        console.log('Request received on path: '+trimedPath+' - Method: '+method+' and with these query parameters', queryStringObject)
+        console.log('Headers of the request', headers)
+    })
 })
 
 // Start the server, and have it listen on port 3000
